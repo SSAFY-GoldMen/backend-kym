96b2cf7124a18c7e12780687d71ff4d666926a6d
Kyongmin1016 <rkdrnwkd1@gmail.com>
2023-12-12 17:37:52 +0900

    feat:initail

---
+++ src/main/java/com/goldmen/cheolbuji/CheolbujiApplication.java
@@ -0,0 +1,13 @@
+package com.goldmen.cheolbuji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheolbujiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheolbujiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goldmen/cheolbuji/CheolbujiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goldmen.cheolbuji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheolbujiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
