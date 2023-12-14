572ab69abcc8efbe37d1e5dd13c8fb0de596cb02
Kyongmin1016 <rkdrnwkd1@gmail.com>
2023-12-14 20:46:50 +0900

    feat:전/ 월세 Domain 및 repository 추가

---
+++ src/main/java/com/goldmen/cheolbuji/domain/realEstate/purchase/jeonse/domain/JeonseRepository.java
@@ -0,0 +1,6 @@
+package com.goldmen.cheolbuji.domain.realEstate.purchase.jeonse.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JeonseRepository extends JpaRepository<Jeonse,Integer> {
+}
---
+++ src/main/java/com/goldmen/cheolbuji/domain/realEstate/purchase/walse/domain/WalseRepository.java
@@ -0,0 +1,6 @@
+package com.goldmen.cheolbuji.domain.realEstate.purchase.walse.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WalseRepository extends JpaRepository<Walse,Integer> {
+}
