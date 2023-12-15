96a4c11360bc9d14b020c29cc2d15bc942e55eb2
Kyongmin1016 <rkdrnwkd1@gmail.com>
2023-12-15 23:02:40 +0900

    feat:global Exception처리 추가

---
+++ src/main/java/com/goldmen/cheolbuji/global/error/ErrorCode.java
@@ -0,0 +1,17 @@
+package com.goldmen.cheolbuji.global.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    // Global
+    API_ERROR_INTERNAL_SERVER(500, "G001", "서버 오류"),
+    API_ERROR_INPUT_INVALID_VALUE(409, "G002", "잘못된 입력"),
+
+    ;
+    private final int status;
+    private final String code;
+    private final String message;
+}
