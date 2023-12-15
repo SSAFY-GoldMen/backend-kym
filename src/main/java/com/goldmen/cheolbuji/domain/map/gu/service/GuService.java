e74471bbad559f6c41b70bb5660f3b08deaa298c
Kyongmin1016 <rkdrnwkd1@gmail.com>
2023-12-15 23:41:37 +0900

    fix: guRepository DI

--- src/main/java/com/goldmen/cheolbuji/domain/map/gu/service/GuService.java
+++ src/main/java/com/goldmen/cheolbuji/domain/map/gu/service/GuService.java
@@ -11,7 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Service
 public class GuService {
-    private GuRepository guRepository;
+    private final GuRepository guRepository;
 
     @Transactional
     public Gu save(Gu gu){
